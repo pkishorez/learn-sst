5ea774c74ae7f9c6a583968fe645f386b46a82b5
Kishore <kishore.iiitn@gmail.com>
2022-08-31 12:31:12 +0530

    feat: Add static react app to stack.

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,6 +1,7 @@
 import { App } from "@serverless-stack/resources";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 import { StorageStack } from "./StorageStack";
 
 export default function main(app: App) {
@@ -11,5 +12,9 @@ export default function main(app: App) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  app // br
+    .stack(StorageStack)
+    .stack(ApiStack)
+    .stack(AuthStack)
+    .stack(FrontendStack);
 }
